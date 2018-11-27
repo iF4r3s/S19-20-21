fea9adf5cfc7e17b315f10c11756e4d0c9052f59
iF4r3s <44950873+iF4r3s@users.noreply.github.com>
2018-11-27 14:57:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ bot19.on('message', message => {
 
 bot19.on('message', message => {
   if(message.content.startsWith(`f-r`)){
-    message.channel.send(`#rep <@505101712839671838>`)
+    message.channel.send(`#rep <@510098846970281984>`)
   }
 });
 
@@ -51,7 +51,7 @@ bot20.on('message', message => {
 
 bot20.on('message', message => {
   if(message.content.startsWith(`f-r`)){
-    message.channel.send(`#rep <@505101712839671838>`)
+    message.channel.send(`#rep <@510098846970281984>`)
   }
 });
 
@@ -87,7 +87,7 @@ bot21.on('message', message => {
 
 bot21.on('message', message => {
   if(message.content.startsWith(`f-r`)){
-    message.channel.send(`#rep <@505101712839671838>`)
+    message.channel.send(`#rep <@510098846970281984>`)
   }
 });
 
